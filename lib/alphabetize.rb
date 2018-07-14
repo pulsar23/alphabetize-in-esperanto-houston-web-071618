0ad1e3a1d2c7a640c031dfbbe77394a5e836616c
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-14 03:23:25 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,7 @@ def alphabetize(arr)
    esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
 
    arr.sort_by do |phrase|
-    phrase.chars.map { |c| esperanto.index(c) }
-  end
+     phrase.chars.map { |c| esperanto.index(c) }
+   end
 
 end
