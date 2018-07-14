c50905b32b0da374ed98b73c3d26222ea2955550
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-14 03:23:36 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
-  # code here
+
+   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
+   arr.sort_by do |phrase|
+     phrase.chars.map { |c| esperanto.index(c) }
+   end
+
 end
