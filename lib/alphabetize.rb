1e8479fecc6a79846ce58acfedfdc92cb1044044
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-14 03:11:46 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(arr)
 
    esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split(//)
 
-   a_list = arr.sort_by { |phrase|  esperanto.index(phrase[0]) }
+   a_list = arr.sort_by |phrase|  esperanto.index(phrase[0]) }
    a_list
 
 end
